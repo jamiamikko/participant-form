4e5db3facb24840728055cab61301484b1707608
jamiamikko <jamiamikko@gmail.com>
2019-09-29 19:28:47 +0300

    Init

---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Content extends Component {
+  render() {
+    return (
+      <section>
+          <p>Tässä content</p>
+      </section>
+    );
+  }
+}
+
+export default Content;
