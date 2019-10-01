2e97e8954b2edc70650aa59f2e50fa40fd7e0da1
jamiamikko <jamiamikko@gmail.com>
2019-10-01 20:06:43 +0300

    Add grid

--- src/participant/ParticipantListFilter.js
+++ src/participant/ParticipantListFilter.js
@@ -4,8 +4,10 @@ import './participant-list.scss';
 class ParticipantListFilter extends Component {
   render() {
     return (
-      <div className="participant-list__filter">
-        <p>Filter</p>
+      <div className="participant-list__filter-wrapper">
+        <button className="participant-list__filter">Name</button>
+        <button className="participant-list__filter">E-mail address</button>
+        <button className="participant-list__filter">Phone number</button>
       </div>
     );
   }
