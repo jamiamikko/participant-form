ab93460f75c0848622fdc6c8cbdecdbb14d99eb6
jamiamikko <jamiamikko@gmail.com>
2019-10-05 20:11:08 +0300

    Edit mode styles

--- src/components/participant/Participant.js
+++ src/components/participant/Participant.js
@@ -17,6 +17,14 @@ class Participant extends Component {
     this.state = this.initialState;
   }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
   toggleEditMode = () => {
     let changeMode = !this.state.editMode;
 
@@ -34,21 +42,46 @@ class Participant extends Component {
           </p>
         </div>
         <div className={`participant__edit ${this.state.editMode ? '' : 'hidden'}`}>
-          <input type="text" name="name" value={this.state.name} />
-          <input type="email" name="email" value={this.state.email} />
-          <input type="tel" name="phone" value={this.state.phone} />
+          <input
+            className="participant__edit-input"
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+          <input
+            className="participant__edit-input"
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+          />
+          <input
+            className="participant__edit-input"
+            type="tel"
+            name="phone"
+            onChange={this.handleChange}
+            value={this.state.phone}
+          />
         </div>
-        <div className="participant__actions">
-          <button className="participant__button">
-            <EditIcon onClick={this.toggleEditMode} />
-          </button>
-          <button
-            className="participant__button"
-            onClick={() => {
-              this.props.deleteParticipant(data.id);
-            }}>
-            <DeleteIcon />
-          </button>
+        <div className="participant__actions-wrapper">
+          <div className={`participant__actions ${!this.state.editMode ? '' : 'hidden'}`}>
+            <button className="participant__icon-button">
+              <EditIcon onClick={this.toggleEditMode} />
+            </button>
+            <button
+              className="participant__icon-button"
+              onClick={() => {
+                this.props.deleteParticipant(data.id);
+              }}>
+              <DeleteIcon />
+            </button>
+          </div>
+          <div className={`participant__actions--edit ${this.state.editMode ? '' : 'hidden'}`}>
+            <button className="participant__edit-button" onClick={this.toggleEditMode}>
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     );
