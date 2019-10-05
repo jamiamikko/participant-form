99df12344086bd3f1ebc6d8ebffc8c52165cd12b
jamiamikko <jamiamikko@gmail.com>
2019-10-05 19:45:18 +0300

    Toggle edit participant

--- src/components/participant/Participant.js
+++ src/components/participant/Participant.js
@@ -4,19 +4,43 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import './participant-list.scss';
 
 class Participant extends Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+      name: this.props.data.name,
+      email: this.props.data.email,
+      phone: this.props.data.phone,
+      editMode: false
+    };
+
+    this.state = this.initialState;
+  }
+
+  toggleEditMode = () => {
+    let changeMode = !this.state.editMode;
+
+    this.setState({ editMode: changeMode });
+  };
+
   render() {
     const data = this.props.data;
 
     return (
       <div className="participant">
-        <div className="participant__info">
+        <div className={`participant__info ${!this.state.editMode ? '' : 'hidden'}`}>
           <p>
             {data.name} - {data.email} {data.phone}
           </p>
         </div>
+        <div className={`participant__edit ${this.state.editMode ? '' : 'hidden'}`}>
+          <input type="text" name="name" value={this.state.name} />
+          <input type="email" name="email" value={this.state.email} />
+          <input type="tel" name="phone" value={this.state.phone} />
+        </div>
         <div className="participant__actions">
           <button className="participant__button">
-            <EditIcon />
+            <EditIcon onClick={this.toggleEditMode} />
           </button>
           <button
             className="participant__button"
