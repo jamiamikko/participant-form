8c1421a3984a9b07ff1274df79bd2756f3d5f155
jamiamikko <jamiamikko@gmail.com>
2019-10-06 20:08:11 +0300

    Sort styles

--- src/components/participant/ParticipantListFilter.js
+++ src/components/participant/ParticipantListFilter.js
@@ -1,36 +1,78 @@
 import React, { Component } from 'react';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import './participant-list.scss';
 
 class ParticipantListFilter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+      sort: {
+        name: false,
+        email: false,
+        phone: false
+      }
+    };
+
+    this.state = this.initialState;
+  }
+
+  activeClass = type => {
+    return this.state.sort[type] ? 'active' : '';
+  };
+
+  sortBy = type => {
+    let newSortState = {
+      name: false,
+      email: false,
+      phone: false
+    };
+
+    newSortState[type] = true;
+
+    this.setState({ sort: newSortState });
+
+    this.props.sortBy(type);
+  };
+
   render() {
     return (
       <thead className="participant-list__filter-wrapper">
         <tr className="participant-list__filter-grid">
           <th>
             <button
-              className="participant-list__filter"
+              className={`participant-list__filter ${this.activeClass('name')}`}
               onClick={() => {
-                this.props.sortBy('name');
+                this.sortBy('name');
               }}>
               Name
+              <ArrowDownwardIcon
+                className={`participant-list__filter-icon ${this.activeClass('name')}`}
+              />
             </button>
           </th>
           <th>
             <button
-              className="participant-list__filter"
+              className={`participant-list__filter ${this.activeClass('email')}`}
               onClick={() => {
-                this.props.sortBy('email');
+                this.sortBy('email');
               }}>
               E-mail address
+              <ArrowDownwardIcon
+                className={`participant-list__filter-icon ${this.activeClass('email')}`}
+              />
             </button>
           </th>
           <th>
             <button
-              className="participant-list__filter"
+              className={`participant-list__filter ${this.activeClass('phone')}`}
               onClick={() => {
-                this.props.sortBy('phone');
+                this.sortBy('phone');
               }}>
               Phone number
+              <ArrowDownwardIcon
+                className={`participant-list__filter-icon ${this.activeClass('phone')}`}
+              />
             </button>
           </th>
         </tr>
